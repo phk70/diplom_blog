571ff3c3b6dfe39f24f45d58dd43c4dcfea8d9fb
koles <yg@htfv.com>
2024-10-12 13:29:20 +0700

    Сделан post_detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . import views
+from .views import post_list, post_detail
 
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', post_list, name='post_list'),
+    path('post/<int:pk>/', post_detail, name='post_detail'),
 ]
 
 
