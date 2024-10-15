705bda0d73a2eab1ef080b57b95951420b96cc7b
koles <yg@htfv.com>
2024-10-15 23:31:51 +0700

    Реализация заполнения профиля, если он отсутствует

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 
 from .forms import NewUserForm
+from .models import Profile
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 
@@ -17,4 +18,14 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == "POST":
+        contact_number = request.POST.get("contact_number")
+        city = request.POST.get("city")
+        sex = request.POST.get("sex")
+        age = request.POST.get("age")
+        image = request.FILES["upload"]
+        user = request.user
+        profile = Profile(user=user, contact_number=contact_number, image=image, city=city, sex=sex, age=age)
+        profile.save()
+        return redirect('users:profile', permanent=True)
+    return render(request, "users/profile.html")
