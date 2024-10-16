875c862ab8e315f360ccc3a2990991e70f31e417
koles <yg@htfv.com>
2024-10-16 21:29:31 +0700

    сделана сортировка постов в ListDetail

--- blog/views.py
+++ blog/views.py
@@ -16,6 +16,7 @@ class PostListView(ListView):
     model = Post
     template_name = "blog/post_list.html"
     context_object_name = "posts"
+    ordering = ['-published_date']
 
 
 # def post_detail(request, pk):
