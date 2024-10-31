b889ea61d47a05276c01858a6856609468904d98
koles <yg@htfv.com>
2024-10-31 21:40:52 +0700

    проблема со статикой. Debug переведен в True

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'   
+# DEBUG = os.getenv('DEBUG', 'False') == 'True'   
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
